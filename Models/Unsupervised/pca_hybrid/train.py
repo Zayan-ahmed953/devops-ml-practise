6fd46dc34476bbe2e5464d50ea11cbf101e14a48
zayan_DevOps <zayan.ahmed@marsdevs.com>
2025-10-20 18:38:51 +0500

    Added pca model

--- Models/Unsupervised/pca_hybrid/train.py
+++ Models/Unsupervised/pca_hybrid/train.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN
@@ -16,6 +17,22 @@ X_pca = pca.fit_transform(X_scaled)
 dbscan = DBSCAN(eps=0.5, min_samples=2)
 labels = dbscan.fit_predict(X_pca)
 
+plt.figure(figsize=(7,5))
+plt.scatter(X_pca[:,0], X_pca[:,1], c=labels, cmap='viridis', s=100, edgecolors='k')
+plt.title("DBSCAN Clustering (PCA-reduced)")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.show()
+
+n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise = list(labels).count(-1)
+
+print("Number of clusters found:", n_clusters)
+print("Number of noise points:", n_noise)
 
 
-
+if n_clusters > 1:
+    score = silhouette_score(X_pca, labels)
+    print("Silhouette Score:", score)
+else:
+    print("Silhouette score not defined (only one cluster or all noise).")
